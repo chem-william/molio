bb6222bff02dd4f82089eb03526536c6b22fb3ea
William Bro-Jørgensen <brojorgensen@proton.me>
2025-04-22 07:32:28 +0200

    add missing residue

---
+++ src/residue.rs
@@ -0,0 +1,28 @@
+use crate::property::Properties;
+use std::collections::BTreeSet;
+
+#[derive(Default, PartialEq, Eq, PartialOrd, Ord)]
+pub struct FullResidueId {
+    /// Chain identifier
+    pub chain: char,
+    /// Residue id
+    pub resid: i64,
+    /// Residue name
+    pub resname: String,
+    /// Insertion code of the residue
+    pub insertion_code: char,
+}
+
+#[derive(Default)]
+pub struct Residue {
+    pub name: String,
+    pub id: Option<i64>,
+    pub atoms: BTreeSet<usize>,
+    pub properties: Properties,
+}
+
+impl Residue {
+    pub fn add_atom(&mut self, index: usize) {
+        self.atoms.insert(index);
+    }
+}
