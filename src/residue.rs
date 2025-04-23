1e79f6090b0c85142d9f535ea547b12d42c0b923
William Bro-Jørgensen <brojorgensen@proton.me>
2025-04-23 11:39:22 +0200

    just derive (partial)ord

--- src/residue.rs
+++ src/residue.rs
@@ -4,7 +4,7 @@ use std::{
     collections::{BTreeSet, btree_set::Iter},
 };
 
-#[derive(Default, PartialEq, Eq, Debug)]
+#[derive(Default, PartialEq, Eq, Debug, PartialOrd, Ord)]
 pub struct FullResidueId {
     /// Chain identifier
     pub chain: char,
@@ -14,22 +14,6 @@ pub struct FullResidueId {
     pub resname: String,
     /// Insertion code of the residue
     pub insertion_code: char,
-}
-
-impl Ord for FullResidueId {
-    fn cmp(&self, other: &Self) -> Ordering {
-        self.chain
-            .cmp(&other.chain)
-            .then_with(|| self.resid.cmp(&other.resid))
-            .then_with(|| self.insertion_code.cmp(&other.insertion_code))
-            .then_with(|| self.resname.cmp(&other.resname))
-    }
-}
-
-impl PartialOrd for FullResidueId {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        Some(self.cmp(other))
-    }
 }
 
 #[derive(Default, Debug, Clone)]
