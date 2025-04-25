50b1110980adb449e082f2ab09de3acb4383847c
William Bro-Jørgensen <brojorgensen@proton.me>
2025-04-25 12:38:41 +0200

    improve criterion benches

--- benches/my_benchmark.rs
+++ benches/my_benchmark.rs
@@ -1,11 +1,16 @@
 use criterion::{Criterion, criterion_group, criterion_main};
 use molio::trajectory::Trajectory;
+use std::hint::black_box;
 use std::path::Path;
 use std::time::Duration;
 
 fn load_trajectory(path: &Path) -> usize {
-    let trajectory = Trajectory::new(path).unwrap();
-    trajectory.size
+    let mut trajectory = Trajectory::new(path).unwrap();
+    let mut dummy = 0;
+    while let Some(next_frame) = trajectory.read().unwrap() {
+        dummy += next_frame.size();
+    }
+    black_box(dummy)
 }
 
 fn criterion_benchmark(c: &mut Criterion) {
@@ -13,6 +18,9 @@ fn criterion_benchmark(c: &mut Criterion) {
     let mut group = c.benchmark_group("my_group");
     group.measurement_time(Duration::from_secs(6));
     group.bench_function("read large xyz", |b| b.iter(|| load_trajectory(path)));
+
+    let path = Path::new("./src/tests-data/pdb/water.pdb");
+    group.bench_function("read large pdb", |b| b.iter(|| load_trajectory(path)));
 }
 
 criterion_group!(benches, criterion_benchmark);
