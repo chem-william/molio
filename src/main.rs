ebd9198548a7f310d98ffbc3bcad9980803368e1
William Bro-Jørgensen <brojorgensen@proton.me>
2025-04-25 15:45:30 +0200

    for better flamegraphs

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,16 @@ use std::{hint::black_box, path::Path};
 use molio::trajectory::Trajectory;
 
 fn main() {
-    let path = Path::new("./helium.xyz");
-    let mut trajectory = Trajectory::new(path).unwrap();
-    let mut dummy = 0;
-    while let Some(next_frame) = trajectory.read().unwrap() {
-        dummy += next_frame.size();
+    // let path = Path::new("./helium.xyz");
+    let path = Path::new("./src/tests-data/xyz/helium.xyz");
+    // let path = Path::new("./src/tests-data/pdb/water.pdb");
+    // let path = Path::new("./water.pdb");
+    for _ in 0..32 {
+        let mut trajectory = Trajectory::new(path).unwrap();
+        let mut dummy = 0;
+        while let Some(next_frame) = trajectory.read().unwrap() {
+            dummy += next_frame.size();
+        }
+        black_box(dummy);
     }
-    black_box(dummy);
 }
