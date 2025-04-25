88dea5989fa7c27b4fefb12cf06c91da6173a604
William Bro-Jørgensen <brojorgensen@proton.me>
2025-04-25 09:48:27 +0200

    attempt some optimization with smallvec

--- src/connectivity.rs
+++ src/connectivity.rs
@@ -1,3 +1,4 @@
+use smallvec::{Array, SmallVec};
 use std::collections::BTreeSet;
 
 use crate::{
@@ -41,25 +42,25 @@ pub struct Connectivity {
 /// bonds/angles/dihedrals/impropers lists by -1
 /// Get the bond order of the bond between `i` and `j`
 impl Connectivity {
-    pub fn angles(&mut self) -> BTreeSet<Angle> {
+    pub fn angles(&mut self) -> &BTreeSet<Angle> {
         if !self.up_to_date {
-            self.recalculate()
+            self.recalculate();
         }
-        self.angles.clone()
+        &self.angles
     }
 
-    pub fn dihedrals(&mut self) -> BTreeSet<Dihedral> {
+    pub fn dihedrals(&mut self) -> &BTreeSet<Dihedral> {
         if !self.up_to_date {
-            self.recalculate()
+            self.recalculate();
         }
-        self.dihedrals.clone()
+        &self.dihedrals
     }
 
-    pub fn impropers(&mut self) -> BTreeSet<Improper> {
+    pub fn impropers(&mut self) -> &BTreeSet<Improper> {
         if !self.up_to_date {
-            self.recalculate()
+            self.recalculate();
         }
-        self.impropers.clone()
+        &self.impropers
     }
 
     pub fn add_bond(&mut self, i: usize, j: usize, bond_order: BondOrder) {
@@ -116,8 +117,10 @@ impl Connectivity {
         self.dihedrals.clear();
         self.impropers.clear();
 
+        // Pre-allocate space for bonded_to vectors
+        let mut bonded_to = vec![SmallVec::<[usize; 4]>::new(); self.biggest_atom + 1];
+
         // Generate the list of which atom is bonded to which one
-        let mut bonded_to = vec![Vec::new(); self.biggest_atom + 1];
         for bond in &self.bonds {
             debug_assert!(bond[0] < bonded_to.len());
             debug_assert!(bond[1] < bonded_to.len());
@@ -129,40 +132,41 @@ impl Connectivity {
         for bond in &self.bonds {
             let i = bond[0];
             let j = bond[1];
-            for k in &bonded_to[i] {
-                if *k != j {
-                    self.angles.insert(Angle::new(*k, i, j));
+
+            for &k in &bonded_to[i] {
+                if k != j {
+                    self.angles.insert(Angle::new(k, i, j));
                 }
             }
 
-            for k in &bonded_to[j] {
-                if *k != i {
-                    self.angles.insert(Angle::new(i, j, *k));
+            for &k in &bonded_to[j] {
+                if k != i {
+                    self.angles.insert(Angle::new(i, j, k));
                 }
             }
         }
 
-        // Generate list of dihedrals
+        // Generate list of dihedrals and impropers
         for angle in &self.angles {
             let i = angle[0];
             let j = angle[1];
             let k = angle[2];
 
-            for m in &bonded_to[i] {
-                if *m != j && *m != k {
-                    self.dihedrals.insert(Dihedral::new(*m, i, j, k));
+            for &m in &bonded_to[i] {
+                if m != j && m != k {
+                    self.dihedrals.insert(Dihedral::new(m, i, j, k));
                 }
             }
 
-            for m in &bonded_to[k] {
-                if *m != i && *m != j {
-                    self.dihedrals.insert(Dihedral::new(i, j, k, *m));
+            for &m in &bonded_to[k] {
+                if m != i && m != j {
+                    self.dihedrals.insert(Dihedral::new(i, j, k, m));
                 }
             }
 
-            for m in &bonded_to[j] {
-                if *m != i && *m != k {
-                    self.impropers.insert(Improper::new(i, j, k, *m));
+            for &m in &bonded_to[j] {
+                if m != i && m != k {
+                    self.impropers.insert(Improper::new(i, j, k, m));
                 }
             }
         }
