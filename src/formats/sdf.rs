52a4aa13f15add2ae9980ae07d2147195f54c7dc
William Bro-Jørgensen <brojorgensen@proton.me>
2025-05-01 12:27:06 +0200

    implement forward

--- src/formats/sdf.rs
+++ src/formats/sdf.rs
@@ -7,7 +7,7 @@
 
 use std::{
     fs::File,
-    io::{BufRead, BufReader, BufWriter},
+    io::{BufRead, BufReader, BufWriter, Seek},
 };
 
 use crate::{error::CError, format::FileFormat, frame::Frame};
@@ -28,7 +28,55 @@ impl FileFormat for SDFFormat {
     }
 
     fn forward(&self, reader: &mut BufReader<File>) -> Result<Option<u64>, CError> {
-        todo!();
+        let position = reader.stream_position()?;
+        let mut line = String::new();
+        let mut read_bytes = 0;
+
+        // Ignore header lines: molecule name, metadata, and general comment line
+        for _ in 0..3 {
+            read_bytes = reader.read_line(&mut line)?;
+            line.clear();
+        }
+
+        if read_bytes == 0 && line.trim().is_empty() {
+            return Ok(None);
+        }
+
+        read_bytes = reader.read_line(&mut line)?;
+        if read_bytes < 10 {
+            return Err(CError::GenericError(format!("counts line must have at least 10 characters in SDF file. It has {read_bytes}: '{line}'")));
+        }
+
+        let natoms = line[..3].trim().parse::<usize>().map_err(|_| {
+            CError::GenericError(format!("could not parse counts line in SDF file: '{line}'"))
+        })?;
+        let nbonds = line[3..6].trim().parse::<usize>().map_err(|_| {
+            CError::GenericError(format!("could not parse counts line in SDF file: '{line}'"))
+        })?;
+
+        for _ in 0..(natoms + nbonds) {
+            read_bytes = reader.read_line(&mut line)?;
+            if read_bytes == 0 && line.is_empty() {
+                return Err(CError::GenericError(
+                    "not enough lines in for SDF format".to_string(),
+                ));
+            }
+            line.clear()
+        }
+
+        // Search for ending character, updating the cursor in the file for the next
+        // call to forward
+        while reader.read_line(&mut line)? > 0 {
+            if line.trim() == "$$$$" {
+                break;
+            }
+            line.clear();
+        }
+
+        // We have enough data to parse an entire molecule.
+        // So, even if the file may not have an ending string,
+        // return the start of this step
+        Ok(Some(position))
     }
 
     fn finalize(&self, writer: &mut BufWriter<File>) -> Result<(), CError> {
@@ -42,8 +90,18 @@ mod tests {
     use crate::trajectory::Trajectory;
 
     #[test]
-    fn check_nsteps() {
+    fn check_nsteps_aspirin() {
         let path = Path::new("./src/tests-data/sdf/aspirin.sdf");
         let trajectory = Trajectory::open(path).unwrap();
+
+        assert_eq!(trajectory.size, 1);
+    }
+
+    #[test]
+    fn check_nsteps_kinases() {
+        let path = Path::new("./src/tests-data/sdf/kinases.sdf");
+        let trajectory = Trajectory::open(path).unwrap();
+
+        assert_eq!(trajectory.size, 6);
     }
 }
